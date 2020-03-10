38330bc16624111ba7502d7ac219950adef0cae3
john alexander agudelo <johnaagudelo@gmail.com>
2020-03-10 17:00:06 -0500

    poc progress download video

--- src/__test__/Counter.test.js
+++ src/__test__/Counter.test.js
@@ -1,33 +1,14 @@
 import React from 'react';
 import { mount } from 'enzyme';
-import toJSON from 'enzyme-to-json';
-
 import Counter from '../components/Counter';
 
 describe('<Counter />', () => {
-  let wrapper;
-  const setState = jest.fn();
-  const useStateSpy = jest.spyOn(React, 'useState');
-  useStateSpy.mockImplementation((init) => [init, setState]);
-
-  beforeEach(() => {
-    wrapper = mount(<Counter />);
-  });
-
-  afterEach(() => {
-    jest.clearAllMocks();
-  });
-
-  it('should render without errors', () => {
-    expect(toJSON(wrapper)).toMatchSnapshot();
-  })
-
-  it('calls setCount with count + 1', () => {
-    const autocomplete = wrapper.find('button[aria-label="Open"]');
+  it('shoult update component', () => {
+    const wrapper = mount(<Counter />);
+    const autocomplete = wrapper.find('input');
     console.log(autocomplete.debug());
-    autocomplete.simulate('click', { target: { name: 'tags', value: 'Toy Story' }});
+    autocomplete.simulate('change', { target: { value: 'The Shawshank Redemption' }});
     wrapper.update();
-    console.log(wrapper.debug());
-    // expect(wrapper.find('p').text()).toBe("1");
+    expect(wrapper.find('p').text()).toEqual('The Shawshank Redemption');
   });
 });
